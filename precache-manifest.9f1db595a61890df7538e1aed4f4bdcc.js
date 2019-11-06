9851174cf5d8cfeaf17da457c334515da5017485
jameslin3263 <kinokojiang@hotmail.com>
2019-11-06 10:27:54 +0800

    Updates

---
+++ precache-manifest.9f1db595a61890df7538e1aed4f4bdcc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8afa69c26e2aa8b737d23f4381d72240",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a89429d7c3810aea1fd8",
+    "url": "/monsters-rolodex/static/css/main.6c1ccc51.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monsters-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "a89429d7c3810aea1fd8",
+    "url": "/monsters-rolodex/static/js/main.c356b9b8.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
